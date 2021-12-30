960ecee1cccb6770cace6554533d52ea30646359
Jacob Degeling <me@jacobdegeling.com>
2021-12-30 22:37:47 +1100

    chore: convert 4 spaces to 2

--- lib/app.rb
+++ lib/app.rb
@@ -1,49 +1,49 @@
 module HowMany
-    class App
-        @@version = "0.0.1"
-        @options = {}
+  class App
+    @@version = "0.0.1"
+    @options = {}
 
-        def self.version
-            @@version
-        end
-
-        def do_calculations
-            raise "till operation not yet implemented" if @options.operation == "till"
-
-            seconds_in = {
-                "second" => 1,
-                "minute" => 60,
-                "hour" => (60*60),
-                "day" => (3600*24),
-                "week" => (3600*24*7),
-                "fortnight" => (3600*24*7*2),
-                "month" => (3600*24*7*(52.1429/12)),
-                "year" => (3600*24*7*52.1429)
-            }
-
-            # calculate the result in seconds
-            result_seconds = @options.from_number * (seconds_in[@options.from_units])
-
-            # unless the units wanted are not secons, display the result,
-            # otherwise calculate the result in whatever units are wanted
-            unless @options.to_units != "seconds"
-                result = result_seconds
-            else
-                result = result_seconds / (seconds_in[@options.to_units])
-            end
-
-            STDOUT.puts @options.type == "integer" ? result.round : result
-        end
-
-        def run(args)
-            begin
-                @options = OptionsParser.parse(args)
-            rescue OptionParser::InvalidArgument, OptionParser::MissingArgument, OptionParser::InvalidOption => exception
-                warn exception.message
-                warn "For usage information, use -h or --help"
-                exit 1
-            end
-            do_calculations
-        end
+    def self.version
+      @@version
     end
+
+    def do_calculations
+      raise "till operation not yet implemented" if @options.operation == "till"
+
+      seconds_in = {
+        "second" => 1,
+        "minute" => 60,
+        "hour" => (60*60),
+        "day" => (3600*24),
+        "week" => (3600*24*7),
+        "fortnight" => (3600*24*7*2),
+        "month" => (3600*24*7*(52.1429/12)),
+        "year" => (3600*24*7*52.1429)
+      }
+
+      # calculate the result in seconds
+      result_seconds = @options.from_number * (seconds_in[@options.from_units])
+
+      # unless the units wanted are not secons, display the result,
+      # otherwise calculate the result in whatever units are wanted
+      unless @options.to_units != "seconds"
+        result = result_seconds
+      else
+        result = result_seconds / (seconds_in[@options.to_units])
+      end
+
+      STDOUT.puts @options.type == "integer" ? result.round : result
+    end
+
+    def run(args)
+      begin
+        @options = OptionsParser.parse(args)
+      rescue OptionParser::InvalidArgument, OptionParser::MissingArgument, OptionParser::InvalidOption => exception
+        warn exception.message
+        warn "For usage information, use -h or --help"
+        exit 1
+      end
+      do_calculations
+    end
+  end
 end
--- lib/options_parser.rb
+++ lib/options_parser.rb
@@ -1,62 +1,62 @@
-require "optparse"
+equire "optparse"
 
 module HowMany
-    class OptionsParser
-        class Options
-            attr_accessor :type, :to_units, :operation, :from_units, :from_number
+  class OptionsParser
+    class Options
+      attr_accessor :type, :to_units, :operation, :from_units, :from_number
 
-            def initialize
-                self.type = :float
-                self.to_units = "second"
-                self.operation = "in"
-                self.from_units = "year"
-                self.from_number = 1
-            end
-        end
+      def initialize
+        self.type = :float
+        self.to_units = "second"
+        self.operation = "in"
+        self.from_units = "year"
+        self.from_number = 1
+      end
+    end
 
-        def self.parse_positional_parameters(args)
-            units_pattern =/(second|minute|hour|day|week|fortnight|month|year)/
+    def self.parse_positional_parameters(args)
+      units_pattern =/(second|minute|hour|day|week|fortnight|month|year)/
 
-            @options ||= Options.new
+      @options ||= Options.new
 
-            @options.to_units= args.shift
-            raise OptionParser::InvalidArgument.new("to units") unless @options.to_units.match units_pattern
+      @options.to_units= args.shift
+      raise OptionParser::InvalidArgument.new("to units") unless @options.to_units.match units_pattern
 
-            @options.operation= args.shift
-            raise OptionParser::InvalidArgument.new("operation") unless @options.operation.match /(in|till)/
+      @options.operation= args.shift
+      raise OptionParser::InvalidArgument.new("operation") unless @options.operation.match /(in|till)/
 
-            @options.from_number= args.shift.to_f
-            raise OptionParser::InvalidArgument.new("number") unless @options.from_number > 0.0
+      @options.from_number= args.shift.to_f
+      raise OptionParser::InvalidArgument.new("number") unless @options.from_number > 0.0
 
-            @options.from_units= args.shift
-            raise OptionParser::InvalidArgument.new("from units") unless @options.from_units.match units_pattern
+      @options.from_units= args.shift
+      raise OptionParser::InvalidArgument.new("from units") unless @options.from_units.match units_pattern
 
-            #strip the 's' from the end of the unit word
-            unless @options.to_units.match /.*s$/
-                @options.to_units= @options.to_units.slice 0, @options.to_units.length - 1
-            end
+      #strip the 's' from the end of the unit word
+      unless @options.to_units.match /.*s$/
+        @options.to_units= @options.to_units.slice 0, @options.to_units.length - 1
+      end
 
-            # remove the trailing s on plural time period names
-            if @options.from_units.match /.*s$/
-                @options.from_units= @options.from_units.slice 0, @options.from_units.length - 1
-            end
+      # remove the trailing s on plural time period names
+      if @options.from_units.match /.*s$/
+        @options.from_units= @options.from_units.slice 0, @options.from_units.length - 1
+      end
 
-            if @options.to_units.match /.*s$/
-                @options.to_units= @options.to_units.slice 0, @options.to_units.length - 1
-            end
-        end
+      if @options.to_units.match /.*s$/
+        @options.to_units= @options.to_units.slice 0, @options.to_units.length - 1
+      end
+    end
 
-        def self.parse(args)
-            @options = Options.new
+    def self.parse(args)
+      @options = Options.new
 
-            args.push('-h') if args.empty?
+      args.push('-h') if args.empty?
 
-            unless args[0].match /^-/
-                self.parse_positional_parameters args
-            end
+      unless args[0].match /^-/
+        self.parse_positional_parameters args
+      end
 
-            @parser ||= OptionParser.new do |parser|
-                parser.banner = <<USAGE
+      @parser ||= OptionParser.new do |parser|
+        parser.banner = <<USAGE
 Usage:
 
 ./how-many <units> in NUMBER <units> [options]
@@ -83,29 +83,29 @@ date        is a date, specified in whatever your local date format is.
 datetime    is a datetime, with date formatted as described as above,
             and time formatted as described above also.
 USAGE
-                parser.separator ""
-                parser.separator "Options:"
+        parser.separator ""
+        parser.separator "Options:"
 
-                parser.on("-tTYPE", "--type=TYPE", ["float", "integer"],
-                          "output type, f for float (default), i for integer") do |type|
+        parser.on("-tTYPE", "--type=TYPE", ["float", "integer"],
+                  "output type, f for float (default), i for integer") do |type|
                     @options.type= type
-                end
+                  end
 
-                parser.separator ""
-                parser.separator "Metadata Options:"
-                parser.on_tail("-h", "--help", "show this message") do
-                    puts parser
-                    exit
-                end
-                parser.on_tail("-V", "--version", "show program version") do
-                    puts App.version
-                    exit
-                end
-            end
-
-            @parser.parse!(args)
-
-            @options
+        parser.separator ""
+        parser.separator "Metadata Options:"
+        parser.on_tail("-h", "--help", "show this message") do
+          puts parser
+          exit
         end
+        parser.on_tail("-V", "--version", "show program version") do
+          puts App.version
+          exit
+        end
+      end
+
+      @parser.parse!(args)
+
+      @options
     end
+  end
 end
