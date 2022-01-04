1e974a8961c75f07e32c1782a40ee366a3d3f106
Jacob Degeling <me@jacobdegeling.com>
2022-01-05 09:35:48 +1100

    test: move help testing to optionsparser spec

--- spec/howmany_spec.rb
+++ spec/howmany_spec.rb
@@ -1,20 +1,6 @@
 require_relative('../lib/app')
 
 RSpec.describe 'how-many' do
-  describe 'displays usage text' do
-    it 'when no args specified' do
-      expect{ system('./how-many') }.to output(a_string_including('Usage:')).to_stdout_from_any_process
-    end
-
-    it 'when --help is specified' do
-      expect{ system('./how-many --help') }.to output(a_string_including('Usage:')).to_stdout_from_any_process
-    end
-
-    it 'when --h is specified' do
-      expect{ system('./how-many -h') }.to output(a_string_including('Usage:')).to_stdout_from_any_process
-    end
-  end
-
   describe 'correctly performs calculations' do
     it 'when correct args passed' do
       expect{ system('./how-many seconds in 1 hour') }.to output("3600.0\n").to_stdout_from_any_process
--- spec/options_parser_spec.rb
+++ spec/options_parser_spec.rb
@@ -1,10 +1,10 @@
+require('spec_helper')
 require_relative('../lib/options_parser')
 
 RSpec.describe HowMany::OptionsParser do
   let(:args) { ['seconds', 'in', '1', 'minute'] }
   let(:args_t) { ['seconds', 'in', '1', 'minute', '-ti'] }
-  let(:args_tf) { ['seconds', 'in', '1', 'minute', '-tf'] }
-  let(:expected_result) { 60 }
+  let(:args_tf) { ['seconds', 'in', '1', 'minute', '--type=f'] }
 
   describe '.parse' do
     it 'parses arguments' do
@@ -25,5 +25,25 @@ RSpec.describe HowMany::OptionsParser do
       result = described_class.parse(args_tf)
       expect(result.type).to eq("float")
     end
+
+    # context 'displays usage text' do
+    #   it 'when no args specified' do
+    #     expect { described_class.parse([]) }.to output(/^Usage:/).to_stdout
+    #   end
+
+    #   it 'when --help is specified' do
+    #     expect { described_class.parse(['--help']) }.to output(/^Usage:/).to_stdout
+    #   end
+
+    #   it 'when -h is specified' do
+    #     expect { described_class.parse(['-h']) }.to output(/^Usage:/).to_stdout
+    #   end
+    # end
+
+    # context 'it works' do
+    #   it 'it really does' do
+    #     expect(8).to eq(8)
+    #   end
+    # end
   end
 end
